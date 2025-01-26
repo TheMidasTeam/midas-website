9660a46173abe6b2fa94895beecf2814b761d2dc
Erick Alvarez <erick505alvarez@gmail.com>
2025-01-26 15:03:23 -0500

    Add Contact Section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,15 +2,17 @@ import OfferingCards from "@/app/components/OfferingCards/OfferingCards";
 import PastWorkSection from "@/app/components/PastWorkSection/PastWorkSection";
 import Components from "@/app/components/Components/Components";
 import FAQs from "@/app/components/FAQ/FAQs";
+import Contact from "@/app/components/Contact/Contact";
 
 export default function Home() {
   return (
     <>
       <div className="wrapper-div px-2 max-w-screen-2xl mx-auto flex flex-col w-full">
-        <Components></Components>
-        <OfferingCards></OfferingCards>
-        <PastWorkSection></PastWorkSection>
-        <FAQs></FAQs>
+        <Components />
+        <OfferingCards />
+        <PastWorkSection />
+        <FAQs />
+        <Contact />
       </div>
     </>
   );
